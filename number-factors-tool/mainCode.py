abb53245c1146698587dd201f9703a99fffd9647
Meow <abodkaramma77@gmail.com>
2025-06-11 09:26:54 +0300

    new update making the code cleaner and making a recursion to give the user more attempts

--- number-factors-tool/mainCode.py
+++ number-factors-tool/mainCode.py
@@ -1,41 +1,59 @@
 print("welcome to prime numbers explorer")
-inputNum = input("num:")
+inputStr = input("num:").strip()
 
 
-def checkIf(num):
+def checkIfInput(string):
+    try:
+        num = int(string)
+        print("Working:")
+        return num
+    except ValueError:
+        print("ERROR: Please enter a valid number")
+        global inputStr
+        inputStr = new_input = input("num:").strip()
+        # Return the result of the recursive call
+        return checkIfInput(new_input)
+
+
+def checkIfNum(num):
     if num > 1000000:
         return False
     return True
 
 
-def getNumberFactores(inputNum):
-    inputNum = int(inputNum.strip())
+def getPrimeFactors(inputStr):
     result = []
-    if checkIf(inputNum):
-        for num in range(1, inputNum):
-            if inputNum % num == 0:
-                result.append(num)
-        result.append(inputNum)
-        return result
-    print("sorry but this number is very big our maxiumum is 1 million")
-
-
-def getPrimeFactores(inputNum):
-    result = []
-    inputNum = int(inputNum.strip())
+    inputStr = int(inputStr)
     i = 2
 
-    while i*i <= inputNum:
-        if inputNum % i == 0:
+    while i*i <= inputStr:
+        if inputStr % i == 0:
 
-            inputNum //= i
+            inputStr //= i
             result.append(i)
         else:
             i += 1
-    if inputNum > 1:
-        result.append(inputNum)
+    if inputStr > 1:
+        result.append(inputStr)
     return result
 
 
-print(getNumberFactores(inputNum))
-print(getPrimeFactores(inputNum))
+def getAllFactors(n):
+    n = int(n)
+    result = []
+    if not checkIfNum(n):
+        print("sorry but this number is very big our maxiumum is 1 million")
+    return [i for i in range(1, n + 1) if n % i == 0]
+    # for num in range(1, inputStr):
+    #     if inputStr % num == 0:
+    #         result.append(num)
+    # result.append(inputStr)
+
+    # return result
+
+
+if checkIfInput(inputStr):
+    all_factors = getAllFactors(inputStr)
+    prime_factors = getPrimeFactors(inputStr)
+    print("All factors:", all_factors)
+    print("Prime factors:", prime_factors)
